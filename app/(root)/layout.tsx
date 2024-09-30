c67022510733fbd54d105fb325475fad0ead1c11
xayuzaii <wizardcodingmachine@gmail.com>
2024-09-30 12:57:52 -0600

    integrated assets

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        {children}
+    </main>
+  );
+}
